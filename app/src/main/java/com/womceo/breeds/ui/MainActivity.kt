109458b6b9b78ffaff024ec54247dddd94902674
Sergio Olmos <63758513+Unshesho@users.noreply.github.com>
2023-07-01 16:39:45 -0400

    initial commit

---
+++ app/src/main/java/com/womceo/breeds/ui/MainActivity.kt
@@ -0,0 +1,69 @@
+package com.womceo.breeds.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
+import com.womceo.breeds.data.BreedsRepository
+import com.womceo.breeds.data.remote.BreedRemoteImpl
+import com.womceo.breeds.data.remote.retrofit.BreedsWebService
+import com.womceo.breeds.ui.theme.WomCeoTheme
+import com.womceo.network.retrofit.WebServiceFactory
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WomCeoTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val api = WebServiceFactory(BreedsWebService::class.java).createRemoteWebServiceConfig()
+        val remote = BreedRemoteImpl(api)
+        val repository = BreedsRepository(remote)
+
+        lifecycleScope.launch {
+            repository.getBreedList().collect { response ->
+                for ((breed, subBreeds) in response.message) {
+                    println("Breed: $breed")
+                    println("Sub-breeds: ${subBreeds.joinToString()}")
+                    println("----------------------")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    WomCeoTheme {
+        Greeting("Android")
+    }
+}
