de13357644331eb1669d61a3c8107881bfe3b940
Sergio Olmos <63758513+Unshesho@users.noreply.github.com>
2023-07-02 03:29:18 -0400

    list viewmodel unit test added

--- app/src/test/java/com/womceo/breeds/data/factory/DogResponseFactory.kt
+++ app/src/test/java/com/womceo/breeds/data/factory/DogResponseFactory.kt
@@ -1,13 +1,11 @@
 package com.womceo.breeds.data.factory
 
 import com.womceo.breeds.data.model.RemoteDogResponse
+import com.womceo.breeds.presentation.list.model.DogResponse
 import com.womceo.utils.randomfactory.RandomFactory.generateRandomString
 
 object DogResponseFactory {
-    fun makeRemoteDogResponse() = RemoteDogResponse(
-        message = makeMapOfMessage(),
-        status = generateRandomString()
-    )
+    private fun makeBreedsList(count: Int) = (0..count).map { generateRandomString() }
 
     private fun makeMapOfMessage() =
         mapOf(
@@ -15,4 +13,12 @@ object DogResponseFactory {
             generateRandomString() to listOf()
         )
 
+    fun makeRemoteDogResponse() = RemoteDogResponse(
+        message = makeMapOfMessage(),
+        status = generateRandomString()
+    )
+
+    fun makeDogResponse(count: Int) = DogResponse(
+        breeds = makeBreedsList(count)
+    )
 }
---
+++ app/src/test/java/com/womceo/breeds/presentation/list/ListViewModelTest.kt
@@ -0,0 +1,94 @@
+package com.womceo.breeds.presentation.list
+
+import com.womceo.breeds.data.BreedsRepository
+import com.womceo.breeds.data.factory.DogResponseFactory.makeDogResponse
+import com.womceo.breeds.data.factory.DogResponseFactory.makeRemoteDogResponse
+import com.womceo.breeds.data.model.RemoteDogResponse
+import com.womceo.breeds.presentation.list.events.ListUIntent.InitialUIntent
+import com.womceo.breeds.presentation.list.events.ListUiStates.DefaultLoadingUiState
+import com.womceo.breeds.presentation.list.events.ListUiStates.DisplayListUiState
+import com.womceo.breeds.presentation.list.events.ListUiStates.ErrorUiState
+import com.womceo.breeds.presentation.list.mapper.ListMapper
+import com.womceo.breeds.presentation.list.model.DogResponse
+import io.mockk.coEvery
+import io.mockk.every
+import io.mockk.mockk
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.last
+import kotlinx.coroutines.flow.take
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+internal class ListViewModelTest {
+    private val mapper = mockk<ListMapper>()
+    private val repository = mockk<BreedsRepository>()
+    private val coroutineContext = Dispatchers.IO
+
+    private val viewModel = ListViewModel(
+        mapper = mapper,
+        repository = repository,
+        coroutineContext = coroutineContext
+    )
+
+    @Test
+    fun `given InitialUserIntent when processUserIntents then emit DefaultLoadingUiState`() =
+        runBlocking {
+            val userIntent = InitialUIntent
+            val remoteDogResponse = makeRemoteDogResponse()
+            stubGetBreedList(remoteDogResponse)
+
+            viewModel.processUserIntents(
+                userIntents = flow { emit(userIntent) },
+                coroutineScope = this
+            )
+
+            val result = viewModel.uiState().first()
+
+            assertTrue(result is DefaultLoadingUiState)
+        }
+
+    @Test
+    fun `given InitialUserIntent when processUserIntents then emit DisplayListUiState`() =
+        runBlocking {
+            val userIntent = InitialUIntent
+            val remoteDogResponse = makeRemoteDogResponse()
+            val dogResponse = makeDogResponse(3)
+            stubGetBreedList(remoteDogResponse)
+            stubMapper(remoteDogResponse, dogResponse)
+
+            viewModel.processUserIntents(
+                userIntents = flow { emit(userIntent) },
+                coroutineScope = this
+            )
+
+            val result = viewModel.uiState().take(2).last()
+
+            assertTrue(result is DisplayListUiState)
+        }
+
+    @Test
+    fun `given error when processUserIntents then emit ErrorUiState`() = runBlocking {
+        val userIntent = InitialUIntent
+        val remoteDogResponse = makeRemoteDogResponse()
+        stubGetBreedList(remoteDogResponse)
+
+        viewModel.processUserIntents(
+            userIntents = flow { emit(userIntent) },
+            coroutineScope = this
+        )
+        val result = viewModel.uiState().take(2).last()
+
+        assertTrue(result is ErrorUiState)
+    }
+
+    private fun stubGetBreedList(remote: RemoteDogResponse) {
+        coEvery { repository.getBreedList() } returns flow { emit(remote) }
+    }
+
+    private fun stubMapper(remote: RemoteDogResponse, presentation: DogResponse) {
+        every { with(mapper) { remote.toPresentation() } } returns presentation
+    }
+}
